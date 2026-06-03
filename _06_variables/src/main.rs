c9ad849d5d4d700eca358d3a5c5caaded0a5c317
jypark <jyp8637282@gmail.com>
2026-06-03 22:35:28 +0900

    06_variables

--- _06_variables/src/main.rs
+++ _06_variables/src/main.rs
@@ -1,3 +1,8 @@
+// Variables and Mutability
 fn main() {
     println!("Hello, world!");
+    let mut a: u16 = 5;
+    println!("The value of a is: {}", a);
+    a = 10;
+    println!("The value of a is: {}", a);
 }
