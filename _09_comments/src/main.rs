2c6ec82a938131357275e34b829dab791eb916d0
jypark <jyp8637282@gmail.com>
2026-06-04 11:03:12 +0900

    09_comments

---
+++ _09_comments/src/main.rs
@@ -0,0 +1,11 @@
+// this is a one line comment
+fn main() {
+    // this is for printing hello, world
+    println!("Hello, world!"); // this is a comment at the end of a line
+
+    // Block comments
+    /*
+    In Rust, you can also write multi-line comments using /* and */.
+    This is a multi-line comment.
+     */
+}
