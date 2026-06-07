e77d8e3c1d07d646a39f2d38694cbafba9e77817
jypark <jyp8637282@gmail.com>
2026-06-07 16:40:49 +0900

    13_enums

---
+++ _13_enums/src/main.rs
@@ -0,0 +1,47 @@
+// Enums
+
+fn main() {
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    enum IpAddrKind{
+        V4,
+        V6
+    }
+
+    fn route(_ip_kind: IpAddrKind){
+        
+    }
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // Using structs
+    enum IpAddr{
+        V4(u8, u8, u8, u8),
+        V6(String)
+    }
+
+    // Using Enums
+    // let home = IpAddr::V4(String::from("127.0.0.1"));
+    // let loopback = IpAddr::V6(String::from("::1"));
+
+    // Enchanced Enums
+    let home = IpAddr::V4(127,0,0,1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // struct IpAddr{
+    //     kind: IpAddrKind,
+    //     address: String
+    // }
+
+    // let home = IpAddr{
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr{
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+}
